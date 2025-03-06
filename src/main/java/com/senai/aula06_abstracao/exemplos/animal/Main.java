91dc7411b01a38f1d5c2767d7b68d1e3acb4dc54
PHCN <pedrohenrique.cn07@gmail.com>
2025-03-06 16:35:14 -0300

    Dia 06/03/2025 exemplos da aula 06 sobre abstração

--- src/main/java/com/senai/aula06_abstracao/exemplos/animal/Main.java
+++ src/main/java/com/senai/aula06_abstracao/exemplos/animal/Main.java
@@ -1,5 +1,8 @@
 package com.senai.aula06_abstracao.exemplos.animal;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         Cachorro cachorro = new Cachorro("Rex");
@@ -10,9 +13,15 @@ public class Main {
         emitirSom(cachorro);
         emitirSom(gato);
         emitirSom(animal);
+
+        List<Animal> listaAnimais = new ArrayList<>();
+
+        listaAnimais.add(cachorro);
+        listaAnimais.add(gato);
+
+        listaAnimais.forEach(Animal :: dormir);
     }
     public static void emitirSom(Animal animal){
-        animal.dormir();
         animal.fazerSom();
     }
 }
