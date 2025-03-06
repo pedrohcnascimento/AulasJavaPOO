e3358a302d97338c84b69c35c731f04ea3e2da36
PHCN <pedrohenrique.cn07@gmail.com>
2025-03-06 15:34:08 -0300

    Dia 06/03/2025 exeplos da aula 06 sobre abstração

--- src/main/java/com/senai/aula06_abstracao/exemplos/veiculos/Main.java
+++ src/main/java/com/senai/aula06_abstracao/exemplos/veiculos/Main.java
@@ -2,8 +2,6 @@ package com.senai.aula06_abstracao.exemplos.veiculos;
 
 public class Main {
     public static void main(String[] args) {
-
-
         Moto moto = new Moto(120);
         Carro carro = new Carro(120);
 
